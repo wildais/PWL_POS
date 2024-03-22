4841ad68ef8b967edcb8fb7336989ed48e9e7bda
wildaimama <wildaimama@gmail.com>
2024-03-22 14:23:54 +0700

    add adminLTE template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,35 @@
-@extends('layouts.app')
+@extends('adminlte::page')
 
-@section('subtitle', 'Welcome')
-@section('content_header_title', 'Home')
-@section('content_header_subtitle', 'Welcome')
+@section('title', 'Dashboard')
 
-{{-- Content body --}}
-@section('content_body')
-    <p>Welcome to this beautiful admin panel</p>
+@section('content_header')
+<h1>Dashboard</h1>
 @stop
 
-{{-- Push extra CSS --}}
-@push('css')
-    
-@endpush
+@section('content')
+    <div class="card-body">
+        <form>
+            <div class="row">
+                <div class="col-sm-6">
+                <!-- text input -->
+                    <div class="form-group">
+                        <label>Level id</label>
+                        <input type="text" class="form-control" placeholder="id">
+                        <div>
+                        </div>
+                        <button type = "submit" class ="btn btn-info">Submit </button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+@stop
 
-{{-- Push extra script --}}
-@push('js')
-    <script>
-        console.log('Hi, I am using AdminLTE package');
-    </script>
-@endpush
+@section('css')
+{{-- Add here extra stylesheets --}}
+{{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+<script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop
